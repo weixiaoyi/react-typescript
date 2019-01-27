4e3fc57f12947a20b4af52614835c055e1af2899
weixiaoyi <2278095567@qq.com>
2019-01-27 18:36:21 +0800

    pretierr

--- src/App.scss.d.ts
+++ src/App.scss.d.ts
@@ -1,3 +1,4 @@
 export const App: string;
 export const app: string;
 export const desc: string;
+export const name: string;
--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,15 @@
-import * as React from 'react';
-import * as styles from './App.scss';
-
+import * as React from "react";
+import * as styles from "./App.scss";
 
 class App extends React.Component {
-    public render() {
-        return (
-            <div className={styles.App} >
-                ggggg
-                <div className={styles.desc} >hshsh</div >
-            </div >
-        );
-    }
+  public render() {
+    return (
+      <div className={styles.App}>
+        gggggggffffffnnnfff
+        <div className={styles.desc}>hshsh</div>
+      </div>
+    );
+  }
 }
 
 export default App;
